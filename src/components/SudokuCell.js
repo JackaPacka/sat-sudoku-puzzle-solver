e32d24ca6cdaa5487ac04e10975d300cb89a1c11
JackaPacka <jr@jackedupsoftware.com>
2024-04-28 23:11:50 -0400

    Update SudokuCell.js

--- src/components/SudokuCell.js
+++ src/components/SudokuCell.js
@@ -5,7 +5,7 @@ function SudokuCell({ value, onChange }) {
     return (
         <input
             className="cell"
-            type="number"
+            type="text"
             maxLength="1"
             value={value}
             onChange={(e) => onChange(e.target.value.replace(/[^1-9]/g, ''))}
